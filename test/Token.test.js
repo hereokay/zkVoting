39d6acb4055953e2c151d9a2f628b51137111e6c
Chris <hereokay88@gmail.com>
2024-05-08 20:45:25 +0900

    test: 권한 부여 및 권한이 없는 계정 mint 실패

--- test/Token.test.js
+++ test/Token.test.js
@@ -5,30 +5,29 @@ const { ethers } = require("ethers");
 
 
 describe("VotingBox contract deployment", function () {
-  let token, votingBox;
-  let owner, addr1, addr2;
+    let token, votingBox;
+    let owner, addr1, addr2;
   
 
+    before(async function () {
+        [owner, addr1, addr2] = await hre.ethers.getSigners();
 
-  beforeEach(async function () {
-    [owner, addr1, addr2] = await hre.ethers.getSigners();
-
-    // 투표토큰 배포
-    token = await hre.ethers.deployContract("Token");
-
-    // 투표관리 컨트랙트 배포
-    votingBox = await hre.ethers.deployContract("VotingBox",[owner.address,token.target]);
-  });
-
-  describe("Token interactions", function () {
-    it("should allow VotingBox to call mint on the Token contract", async function () {
-      // Example interaction
-
-
-      await token.mint(addr1.address, ethers.parseEther("1"));
-      expect(await token.balanceOf(addr1.address)).to.equal(ethers.parseEther("1"));
-      
+        // 투표토큰 배포
+        token = await hre.ethers.deployContract("Token");
+        // 투표관리 컨트랙트 배포
+        votingBox = await hre.ethers.deployContract("VotingBox",[owner.address,token.target]);
     });
-    
-  });
+
+    describe("Token", function () {
+        it("VotingBox에게 mint 권한부여", async function () {
+            await token.setOwner(votingBox.target);
+            expect(await token.owner()).to.equal(votingBox.target);
+        });
+
+        it("권한없는 사람은 mint 실패", async function () {
+            await expect(token.mint(addr1.address, ethers.parseEther("1")))
+            .to.be.revertedWith("Sender not authorized.");  
+        });
+    });
+
 });
