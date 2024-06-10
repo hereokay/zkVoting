77716283535b74efed9abe32598d3045589ec44a
Chris <hereokay88@gmail.com>
2024-06-10 19:57:56 +0900

    feat(registCandidate) : 후보자 온체인 등록

--- scripts/registCandidate.js
+++ scripts/registCandidate.js
@@ -18,14 +18,24 @@ const {
 
 // 후보자를 등록하는 과정
 async function main() {
-    const Tokenaddress = "0x1539d89CA6C8a5E395967AAB7176A4Ba21D99280"
-    const VotingBoxaddress = "0x2832B0e3cd778e3991933FbBcCD5FbEbE651Fe24"
-    const Tornadoaddress = "0xe6f20e4f874272d446E3b51B411167009095B707"
+    const Tokenaddress = "0x4C8c9967f90E0C66452ca3577639187A3b3e9eF3"
+    const VotingBoxaddress = "0xD4d6A26CdeAb40F61C430aB25baEb2C0c23b88aD"
+    const Tornadoaddress = "0xF93da63aC52634D1996AC0dE7EdFd3fa5dcF5cd6"
+
+
+    const cadidateList = [
+      '0x85E6cC88F3055b589eb1d4030863be2CFcc0763E',
+      '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+      '0x5BD63a7ECc13b955C4F57e3F12A64c10263C14c1'
+    ]
 
     const votingBox = await hre.ethers.getContractAt("VotingBox", VotingBoxaddress);
     const token = await hre.ethers.getContractAt("Token", Tokenaddress);
     const tornado = await hre.ethers.getContractAt("Tornado", Tornadoaddress);
 
+    // await votingBox.addCandidate(1, cadidateList[0]);
+    // await votingBox.addCandidate(2, cadidateList[1]);
+    await votingBox.addCandidate(0, cadidateList[2]);
 
 }
 
@@ -38,3 +48,5 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+/// 19970542151702934369367482590522924987019939050486916236202599119463928648810,0x8F1864674a91A4147c4661f5Dd4190fAa358A0D5
